459561dd520d3fb960bf73daa88209fc28e2c3cc
iarakelly <iarakelly.ba@gmail.com>
2026-03-30 18:15:20 -0300

    intro knn in java, serial version

---
+++ src/main.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Point{
+    double[] features;
+    String label;
+
+    public Point(double[] features, String label){
+        this.features = features;
+        this.label = label;
+    }
+
+}
+
+public class knn {
+
+    public void main CalcularDistancia(Point p1, Point p2){
+        double sum = 0.0;
+        for (i=0; i<p1.leangth; i++){
+            sum += Math.pow(p1.features[i]-p2.features[i], 2);
+
+            return Math.sqrt(sum);
+        }
+    }
+}
+
+public class main {
+    =
+}
